ae010c57d38df8701d2b8342081d7c1903cd40bd
OdysseaD <ulyssedemonio@gmail.com>
2019-11-15 22:25:32 +0100

    Code to split dataset in training and test set

---
+++ Splitting dataset.R
@@ -0,0 +1,7 @@
+#DIVIDING DATA IN TRAINING AND TEST SET
+
+df_congress$id <- 1:nrow(df_congress)
+train <- df_congress %>% dplyr::sample_frac(.75)
+test  <- dplyr::anti_join(df_congress, train, by = 'id')
+View(df_congress)
+View(train)
