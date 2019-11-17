efdcb6d6ef4af33624fd6da3b83d004ac7265bab
Gaetano429 <perdoux.gaetan@gmail.com>
2019-11-17 20:08:19 +0100

    spliting

--- 02.Splitting dataset.R
+++ 02.Splitting dataset.R
@@ -1,11 +1,10 @@
 #DIVIDING DATA IN TRAINING AND TEST SET
 
-df_congress$id <- 1:nrow(df_congress)
-train <- df_congress %>% dplyr::sample_frac(.75)
-test  <- dplyr::anti_join(df_congress, train, by = 'id')
+congress_wordscore$id <- 1:nrow(congress_wordscore)
+train_set <- congress_wordscore %>% dplyr::sample_frac(.75)
+test_set  <- dplyr::anti_join(congress_wordscore, train_set, by = 'id')
 
 #ORDERING DATA ALPHABETICALLY
-train <- with(train,train[order(Politician),])
-
-View(train)
-
+train_set <- with(train_set,train_set[order(document),])
+test_set <- with(test_set,test_set[order(document),])
+ 
