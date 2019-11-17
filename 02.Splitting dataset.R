7d9c6577d51cb41616ef556a0fe88cb624d4fe53
Gaetano429 <perdoux.gaetan@gmail.com>
2019-11-17 20:07:02 +0100

    split in two sets

--- 02.Splitting dataset.R
+++ 02.Splitting dataset.R
@@ -3,9 +3,9 @@
 df_congress$id <- 1:nrow(df_congress)
 train <- df_congress %>% dplyr::sample_frac(.75)
 test  <- dplyr::anti_join(df_congress, train, by = 'id')
-View(train)
 
 #ORDERING DATA ALPHABETICALLY
 train <- with(train,train[order(Politician),])
+
 View(train)
 
