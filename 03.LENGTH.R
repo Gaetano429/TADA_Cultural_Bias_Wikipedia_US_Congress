74daf0128e2b1207e7812d4c92454682f8351de7
Gaetano429 <perdoux.gaetan@gmail.com>
2019-11-18 11:45:17 +0100

    Length of tokens code

---
+++ 03.LENGTH.R
@@ -0,0 +1,16 @@
+#GET LENGTH (OF TOKENS) THROUGH XCL
+
+library(csv)
+my.summary<- summary(congress_tok)
+length_df<-data.frame(ids=length(my.summary), length=my.summary)
+write.csv(length_df, "C:/Users/gaeta/TADA_Cultural_Bias_Wikipedia_US_Congress/length.csv",row.names = FALSE)
+
+
+length <- read_excel("length.xlsx")
+
+
+#join the length to the data frame
+
+df_congress<-dplyr::left_join(x = df_congress,
+                              y = length,
+                              by = "Politician")
