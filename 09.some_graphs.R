4754a0873b416a2e97b6841e307bb603c823b633
Gaetano429 <perdoux.gaetan@gmail.com>
2019-11-21 19:14:31 +0100

    mini edits

--- 09.some_graphs.R
+++ 09.some_graphs.R
@@ -4,8 +4,7 @@
 library(ggplot2)
 ggplot(data=congress_wordscore2, aes(x=congress_wordscore2$democrat_score2, y=congress_wordscore2$republican_score2)) +
   geom_point(aes(color=party_affiliation), alpha=1, shape=4, stroke=2) +
-  xlab("Democrat score") +
-  ylab("Republic score")
+  xlab("Democrat score") +  ylab("Republican score") 
 
 
 #Simple Scatterplot
@@ -20,7 +19,7 @@ plot(congress_wordscore2$length,congress_wordscore2$republican_score2, main="Sca
      xlab="length", ylab="rep score")
 lines(lowess(congress_wordscore2$length,congress_wordscore2$republican_score2), col="red") 
 
-plot(congress_wordscore2$total_editors, congress_wordscore2$democrat_score2, main="Scatterplot editors/dem_score2",
+plot(congress_wordscore2$total_editors, congress_wordscore2$democrat_score2, main="Scatterplot editors/demscore2",
      xlab="editors", ylab="dem score")
 lines(lowess(congress_wordscore2$total_editors,congress_wordscore2$democrat_score2), col="blue") 
 
