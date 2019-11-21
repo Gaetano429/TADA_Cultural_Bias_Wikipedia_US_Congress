19b329e34b062c7d31430c9d1b18346b807e8f2f
Gaetano429 <perdoux.gaetan@gmail.com>
2019-11-21 18:59:04 +0100

    cleaning

--- 09.some_graphs.R
+++ 09.some_graphs.R
@@ -12,20 +12,19 @@ ggplot(data=congress_wordscore2, aes(x=congress_wordscore2$democrat_score2, y=co
 
 
 
-plot(congress_wordscore2$length,congress_wordscore2$democrat_score2)
-lines(lowess(congress_wordscore2$length,congress_wordscore2$democrat_score2), col="red") 
+plot(congress_wordscore2$length,congress_wordscore2$democrat_score2, main="Scatterplot length/democratscore2",
+     xlab="length", ylab="dem score")
+lines(lowess(congress_wordscore2$length,congress_wordscore2$democrat_score2), col="blue") 
 
-plot(congress_wordscore2$length,congress_wordscore2$republican_score2)
+plot(congress_wordscore2$length,congress_wordscore2$republican_score2, main="Scatterplot length/republicanscore2",
+     xlab="length", ylab="rep score")
 lines(lowess(congress_wordscore2$length,congress_wordscore2$republican_score2), col="red") 
 
-plot(congress_wordscore2$total_editors, congress_wordscore2$democrat_score2, main="Scatterplot editors_dem_score",
+plot(congress_wordscore2$total_editors, congress_wordscore2$democrat_score2, main="Scatterplot editors/dem_score2",
      xlab="editors", ylab="dem score")
-abline(lm(congress_wordscore2$total_editors~congress_wordscore2$democrat_score2), col="red") 
 lines(lowess(congress_wordscore2$total_editors,congress_wordscore2$democrat_score2), col="blue") 
-
 
 plot(congress_wordscore2$total_editors, congress_wordscore2$republican_score2, main="Scatterplot editors rep score",
      xlab="editors", ylab="rep score")
-abline(lm(congress_wordscore2$total_editors~congress_wordscore2$republican_score2), col="red") 
-lines(lowess(congress_wordscore2$total_editors,congress_wordscore2$republican_score2), col="blue") 
+lines(lowess(congress_wordscore2$total_editors,congress_wordscore2$republican_score2), col="red") 
 
